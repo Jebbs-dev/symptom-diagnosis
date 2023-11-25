b314822a771a470224967ec6119295a6907fd607
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-25 12:37:17 +0100

    feat: Add diagnosis checking history
    Added a section to give overview of recent diagnosis and check diagnosis history

---
+++ src/modules/landing/history/data/dignosis-data.ts
@@ -0,0 +1,74 @@
+const today: Date = new Date();
+const yesterday: Date = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // January is 0, so we add 1
+const day = today.getDate();
+
+const options: Intl.DateTimeFormatOptions = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+const formattedDate = `${month}-${day}-${year}`;
+const stringDate = today.toLocaleDateString("en-US", options);
+const yesterdayDate = `${month}-${day}-${year}`;
+const yesterdayString = yesterday.toLocaleDateString("en-US", options);
+
+
+export const report = [
+  {
+    id: 1,
+    title: "Malaria Fever",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: yesterdayString,
+    datetime: yesterdayDate,
+    category: { title: "mild" },
+    author: {
+      name: "Michael Foster",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "Indigestion",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "September 21, 2023",
+    datetime: "2023-09-21",
+    category: { title: "severe" },
+    author: {
+      name: "Sean Bryce",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 1,
+    title: "Common cold",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "March 13, 2023",
+    datetime: "2023-03-13",
+    category: { title: "mild" },
+    author: {
+      name: "John Hopkins Hospital",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  // More posts...
+];

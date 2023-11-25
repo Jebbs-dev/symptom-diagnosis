4cd7f731c03bb2d5a6df0235dc135923ce119132
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-25 12:14:54 +0100

    ci: change config file
    Added custom styling and structural preferences

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    "src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      gridTemplateColumns: {
+        footer: "2fr repeat(2, 1.2fr) 1.5fr  2fr",
+        cart: "1fr 2fr 1fr",
+        diagnosis: "2fr 1fr"
+      },
+      fontFamily: {
+        cabin: ["Cabin", "sans-serif"],
+        montserrat: ["Montserrat"],
+      },
+      screens: {
+        'xm': '350px',
+        // => @media (min-width: 992px) { ... }
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

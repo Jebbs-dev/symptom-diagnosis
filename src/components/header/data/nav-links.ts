9349f0cf9d5f2a659b4b4a52563e53d7a09efa5e
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-25 12:32:48 +0100

    feat: Add navigation bar

---
+++ src/components/header/data/nav-links.ts
@@ -0,0 +1,6 @@
+export const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Explore', href: '#explore', current: false },
+  { name: 'Features', href: '#features', current: false },
+  { name: 'Diagnosis', href: '#diagnosis', current: false },
+]

f2a23c0caf0708ab76b4cba46b4f618e19733b71
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-25 12:29:28 +0100

    feat: Add information section
    Added information section and the data to be displayed there

---
+++ src/modules/landing/infosection/data/section-data.ts
@@ -0,0 +1,26 @@
+export const sectionData = [
+  {
+    heading: "Analyze your symptoms",
+    alt: false,
+    description:
+      "Experience the future of healthcare with our AI-Powered Symptom and Diagnosis Guidance, enabling a meticulous and personalized examination of your symptoms.",
+    img: "",
+    id:"analyze"
+  },
+  {
+    heading: "Diagnose your condition",
+    alt: true,
+    description:
+      "Unlock precise healthcare solutions through the power of AI, with a thorough examination, delivering comprehensive insights into your condition.",
+    img: "",
+    id:"diagnose"
+  },
+  {
+    heading: "Maintain your health",
+    alt: false,
+    description:
+      "Effortlessly track and manage your health parameters, gain personalized strategies and proactive tips to ensure a sustainable and empowered approach to maintaining your well-being.",
+    img: "",
+    id:"maintain"
+  },
+];

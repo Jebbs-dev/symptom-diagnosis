fed86eac1cf97c0486cc436107a7df4787624b19
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-27 18:00:00 +0100

    add: Update logic and styles for signin

--- src/components/forms/signin/signin.tsx
+++ src/components/forms/signin/signin.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import React, { useState, useRef, Fragment, Dispatch, SetStateAction } from "react";
+import React, {
+  useState,
+  useRef,
+  Fragment,
+  Dispatch,
+  SetStateAction,
+} from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { XMarkIcon } from "@heroicons/react/24/outline";
@@ -10,12 +16,22 @@ interface IFormInput {
   password: string;
 }
 
-export const Signin = ({ showSignin }: any) => {
+type SiginProps = {
+  showSignin: boolean,
+  createAccount: React.MouseEventHandler<HTMLAnchorElement>,
+}
+
+export const Signin = ({ showSignin, createAccount }: SiginProps) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IFormInput>();
+  } = useForm<IFormInput>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
@@ -36,7 +52,7 @@ export const Signin = ({ showSignin }: any) => {
               id="signin"
               className="relative z-10"
               initialFocus={cancelButtonRef}
-              onClose={()=>setOpen(false)}
+              onClose={() => setOpen(false)}
             >
               <Transition.Child
                 as={Fragment}
@@ -62,9 +78,14 @@ export const Signin = ({ showSignin }: any) => {
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                   >
                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-5 lg:px-8">
+                      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-5 lg:px-5">
                         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                          <div className="ml-3 relative flex flex-col-reverse h-7 items-end">
+                          <div className="relative flex justify-between items-end">
+                          <img
+                            className="h-10 w-auto"
+                            src="logo.png"
+                            alt="Your Company"
+                          />
                             <button
                               type="button"
                               className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
@@ -78,18 +99,13 @@ export const Signin = ({ showSignin }: any) => {
                               />
                             </button>
                           </div>
-
-                          <img
-                            className="mx-auto h-10 w-auto"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                            alt="Your Company"
-                          />
-                          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                          
+                          <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                             Sign in to your account
                           </h2>
                         </div>
 
-                        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
                           <form
                             className="space-y-6"
                             onSubmit={handleSubmit((d) => console.log(d))}
@@ -110,18 +126,18 @@ export const Signin = ({ showSignin }: any) => {
                                   {...register("email", {
                                     required: "Email is required",
                                     validate: {
-                                      pattern:
-                                        regex.test ||
+                                      matchPattern: (v) =>
+                                        regex.test(v) ||
                                         "Please input a valid email",
                                       maxLength: (v) =>
                                         v.length <= 50 ||
                                         "Email cannot be more than 50 characters",
                                     },
                                   })}
-                                  className="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  className="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 />
                                 {errors.email?.message && (
-                                  <small>{errors.email.message}</small>
+                                  <small className="text-red-500">{errors.email.message}</small>
                                 )}
                               </div>
                             </div>
@@ -137,7 +153,7 @@ export const Signin = ({ showSignin }: any) => {
                                 <div className="text-sm">
                                   <a
                                     href="#"
-                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                                    className="font-semibold text-violet-600 hover:text-violet-500"
                                   >
                                     Forgot password?
                                   </a>
@@ -149,16 +165,24 @@ export const Signin = ({ showSignin }: any) => {
                                   placeholder="••••••••"
                                   type="password"
                                   autoComplete="current-password"
-                                  {...register("password", { minLength: 4 })}
-                                  className="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                  {...register("password", {
+                                    required: "Password is required",
+                                    validate: {
+                                      minLength: (v)=> v.length >= 9 || "Password must not be less than 8 characters"
+                                    },
+                                  })}
+                                  className="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 />
+                                {errors.password?.message && (
+                                  <small className="text-red-500">{errors.password.message}</small>
+                                )}
                               </div>
                             </div>
 
                             <div>
                               <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
                               >
                                 Sign in
                               </button>
@@ -166,12 +190,13 @@ export const Signin = ({ showSignin }: any) => {
                           </form>
 
                           <p className="mt-10 text-center text-sm text-gray-500">
-                            Not a member?{" "}
+                            Dont have an account?{" "}
                             <a
                               href="#"
-                              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                              onClick={createAccount}
+                              className="font-semibold leading-6 text-violet-600 hover:text-violet-500"
                             >
-                              Start a 14 day free trial
+                              Create an Account
                             </a>
                           </p>
                         </div>

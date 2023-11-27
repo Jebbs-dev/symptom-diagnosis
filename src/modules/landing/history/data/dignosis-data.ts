435b7672cb92a28322ab89bb8028bc50f0fb8d25
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-27 17:47:06 +0100

    add: Add dummy data for diagnosis history

--- src/modules/landing/history/data/dignosis-data.ts
+++ src/modules/landing/history/data/dignosis-data.ts
@@ -37,7 +37,7 @@ export const report = [
     },
   },
   {
-    id: 1,
+    id: 2,
     title: "Indigestion",
     href: "#",
     description:
@@ -54,7 +54,109 @@ export const report = [
     },
   },
   {
-    id: 1,
+    id: 3,
+    title: "Common cold",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "March 13, 2023",
+    datetime: "2023-03-13",
+    category: { title: "mild" },
+    author: {
+      name: "John Hopkins Hospital",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 4,
+    title: "Malaria Fever",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: yesterdayString,
+    datetime: yesterdayDate,
+    category: { title: "mild" },
+    author: {
+      name: "Michael Foster",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 5,
+    title: "Indigestion",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "September 21, 2023",
+    datetime: "2023-09-21",
+    category: { title: "severe" },
+    author: {
+      name: "Sean Bryce",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 6,
+    title: "Common cold",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "March 13, 2023",
+    datetime: "2023-03-13",
+    category: { title: "mild" },
+    author: {
+      name: "John Hopkins Hospital",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 7,
+    title: "Malaria Fever",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: yesterdayString,
+    datetime: yesterdayDate,
+    category: { title: "mild" },
+    author: {
+      name: "Michael Foster",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 8,
+    title: "Indigestion",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "September 21, 2023",
+    datetime: "2023-09-21",
+    category: { title: "severe" },
+    author: {
+      name: "Sean Bryce",
+      role: "Recommended Doctor / Hospital",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 9,
     title: "Common cold",
     href: "#",
     description:

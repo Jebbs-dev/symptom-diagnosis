e4d752bf32f64a0b3137e928f31f17f81945b89b
Jebbs-dev <ojebiyifulness@gmail.com>
2023-11-30 06:57:31 +0100

    style: Format styling and add images

--- src/modules/landing/infosection/data/section-data.ts
+++ src/modules/landing/infosection/data/section-data.ts
@@ -4,7 +4,7 @@ export const sectionData = [
     alt: false,
     description:
       "Experience the future of healthcare with our AI-Powered Symptom and Diagnosis Guidance, enabling a meticulous and personalized examination of your symptoms.",
-    img: "",
+    img: "/images/steth.jpg",
     id:"analyze"
   },
   {
@@ -12,7 +12,7 @@ export const sectionData = [
     alt: true,
     description:
       "Unlock precise healthcare solutions through the power of AI, with a thorough examination, delivering comprehensive insights into your condition.",
-    img: "",
+    img: "/images/min-see.jpg",
     id:"diagnose"
   },
   {
@@ -20,7 +20,7 @@ export const sectionData = [
     alt: false,
     description:
       "Effortlessly track and manage your health parameters, gain personalized strategies and proactive tips to ensure a sustainable and empowered approach to maintaining your well-being.",
-    img: "",
+    img: "/images/health-check.jpg",
     id:"maintain"
   },
 ];
--- src/modules/landing/infosection/infosection.tsx
+++ src/modules/landing/infosection/infosection.tsx
@@ -11,7 +11,7 @@ export const InfoSection = () => {
           <div
             key={data.id}
             id="explore"
-            className="overflow-hidden bg-gray-200 py-20 sm:py-32"
+            className="overflow-hidden bg-gray-200 py-16 sm:py-24"
           >
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto grid items-center max-w-2xl grid-cols-1 gap-x-8 gap-y-10 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
@@ -19,7 +19,7 @@ export const InfoSection = () => {
                   className={`${data.alt ? "order-2" : ""} lg:pr-8 lg:pt-2 `}
                 >
                   <div className="lg:max-w-lg">
-                    <h2 className="text-3xl font-semibold leading-7 text-gray-900 sm:text-3xl font-montserrat">
+                    <h2 className="text-3xl font-semibold leading-7 text-gray-900 sm:text-3xl">
                       {data.heading}
                     </h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -28,22 +28,12 @@ export const InfoSection = () => {
                   </div>
                 </div>
                 <div>
-                  {/* <img
-                  src="https://images.unsplash.com/photo-1583912086296-be5b665036d3?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="Product screenshot"
-                  className={`${
-                    data.alt
-                      ? "order-1 right-40"
-                      : " md:-ml-4 lg:-ml-0"
-                  }  rounded-xl max-w-xl max-h-none h- shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]`}
-                  width={700}
-                  height={700}
-                /> */}
                   <Image
-                    src="https://images.unsplash.com/photo-1505751172876-fa1923c5c528?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src={data.img}
                     width={600}
                     height={600}
                     alt=""
+                    className="rounded-lg"
                   />
                 </div>
               </div>
